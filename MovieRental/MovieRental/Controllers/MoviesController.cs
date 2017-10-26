f788eeb406236116a62aedea15dc22de54ff413d
Dariusz Gorski <dariusz.gorski@comarch.pl>
2017-10-26 13:48:06 +0200

    Added correct MoviesController and Details in View/Movies

--- MovieRental/MovieRental/Controllers/MoviesController.cs
+++ MovieRental/MovieRental/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,41 +11,39 @@ namespace MovieRental.Controllers
 {
     public class MoviesController : Controller
     {
-        readonly List<Movie> _movies = new List<Movie>
+        private ApplicationDbContext _context;
+
+        public MoviesController()
         {
-            new Movie { Id = 1,Name = "Shrek" },
-            new Movie {Id = 2, Name = "Wall-E"}
-        };
-        readonly List<Customer> _customers = new List<Customer>
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
         {
-            new Customer() {Id=1,Name = "John Smith"},
-            new Customer() {Id=2,Name = "Mary Williams"}
-        };
+            _context.Dispose();
+        }
+
         // GET: Movies/Random
         public ActionResult Index()
         {
 
-            RandomMovieViewModel viewModel = new RandomMovieViewModel
-            {
-                Movies = _movies,
-                Customers = _customers
-            };
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
 
-            return View(viewModel);
+            return View(movies);
         }
 
         //EDIT id = id
-        public ActionResult Edit(int id)
+        public ActionResult Details(int id)
         {
-            return Content("id= " + id);
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
         }
 
         //movies
 
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
+        
     }
 }
