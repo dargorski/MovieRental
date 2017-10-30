d060d22d563fd064e7028d088b958c6e8b180f9a
Dariusz Gorski <dariusz.gorski@comarch.pl>
2017-10-30 22:51:08 +0100

    Created EDIT page

--- MovieRental/MovieRental/Controllers/CustomersController.cs
+++ MovieRental/MovieRental/Controllers/CustomersController.cs
@@ -52,11 +52,11 @@ namespace MovieRental.Controllers
         public ActionResult NewCustomer()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);
+            return View("CustomerForm",viewModel);
         }
 
         [HttpPost]
@@ -66,6 +66,23 @@ namespace MovieRental.Controllers
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+
         }
     }
 }
