8d2b1834fa4dca2f98b681ed002a0ea9d51c5287
Dariusz Gorski <dariusz.gorski@comarch.pl>
2017-11-06 11:27:17 +0100

    Created Edit Movie page

--- MovieRental/MovieRental/Controllers/MoviesController.cs
+++ MovieRental/MovieRental/Controllers/MoviesController.cs
@@ -83,5 +83,22 @@ namespace MovieRental.Controllers
 
             return RedirectToAction("Index", "Movies");
         }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+
+        }
     }
 }
