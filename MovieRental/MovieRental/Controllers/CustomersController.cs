1df1f2b387444336e38bc1e53dc2a8147fb50225
Dariusz Gorski <dariusz.gorski@comarch.pl>
2017-11-06 12:48:01 +0100

    Added validation to CustomerForm

--- MovieRental/MovieRental/Controllers/CustomersController.cs
+++ MovieRental/MovieRental/Controllers/CustomersController.cs
@@ -65,6 +65,17 @@ namespace MovieRental.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
 
             if (customer.Id == 0)
             {
